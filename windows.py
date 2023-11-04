6184ca20b998313b0f34d28613e6a059a24a42c6
xNewHumanx <kol.i.sem@yandex.ru>
2023-11-05 01:03:47 +0300

    new file

--- windows.py
+++ windows.py
@@ -1,1 +1,11 @@
-print('Hello world')
+import sys
+from PyQt6.QtWidgets import QApplication, QDialog
+from ui_imagedialog import Ui_MainWindow
+
+app = QApplication(sys.argv)
+window = QDialog()
+ui = Ui_MainWindow()
+ui.setupUi(window)
+
+window.show()
+sys.exit(app.exec())
